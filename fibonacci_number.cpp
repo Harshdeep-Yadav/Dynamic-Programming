67a73144787d6fdef1e0f1beee19e06bf86175a2
Harshdeep Yadav <harshdeepy5708@gmail.com>
2022-07-22 23:54:48 +0530

    initial commit

--- fibonacci_number.cpp
+++ fibonacci_number.cpp
@@ -21,3 +21,44 @@ int main()
 
 }
 
+#include <bits/stdc++.h>
+
+using namespace std;
+//``````````````````````````````````````    Part -2: Tabulation````````````````````````````````
+
+int main() {
+
+  int n=5;
+  vector<int> dp(n+1,-1);
+  
+  dp[0]= 0;
+  dp[1]= 1;
+  
+  for(int i=2; i<=n; i++){
+      dp[i] = dp[i-1]+ dp[i-2];
+  }
+  cout<<dp[n];
+  return 0;
+}
+
+ // ````````````````````````````````````````Part 3: Space Optimization``````````````````````
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main() {
+
+  int n=5;
+  
+  int prev2 = 0;
+  int prev = 1;
+  
+  for(int i=2; i<=n; i++){
+      int cur_i = prev2+ prev;
+      prev2 = prev;
+      prev= cur_i;
+  }
+  cout<<prev;
+  return 0;
+}
