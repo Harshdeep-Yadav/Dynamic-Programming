18a658347fcea580ba97972b8109cf41bc7cfffa
Harshdeep Yadav <harshdeepy5708@gmail.com>
2022-07-26 14:41:04 +0530

    initial commit

--- triangle_min_path_sum.cpp
+++ triangle_min_path_sum.cpp
@@ -31,3 +31,40 @@ int minimumPathSum(vector<vector<int>>& triangle, int n){
 
 //````````````tabulation solution````````````````````
 
+int minimumPathSum(vector<vector<int>>&triangle,int n)
+{
+    int dp[n][n];
+    for(j=0;j<n;j++){
+        dp[n-1][j]=triangle[n-1][j];
+    }
+    for(int i=n-2;i>=0;i--){
+        for(int j=i;j>=0;j--){
+            int down=triangle[i][j]+dp[i+1][j];
+            int digonal=triangle[i][j]+dp[i+1][j+1]
+            dp[i][j]=min(down,digonal);
+        }
+    }
+    return dp[0][0];
+}
+
+// `````````space optimizatoin`````````
+
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+    int n=triangle.size();
+   vector<int> front(n,0),cur(n,0);
+    for(int j=0;j<n;j++){
+        front[j]=triangle[n-1][j];
+    }
+    for(int i=n-2;i>=0;i--){
+        for(int j=i;j>=0;j--){
+            int down=triangle[i][j]+front[j];
+            int digonal=triangle[i][j]+front[j+1];
+            cur[j]=min(down,digonal);
+        }
+        front=cur;
+    }
+    return front[0];
+    }
+};
