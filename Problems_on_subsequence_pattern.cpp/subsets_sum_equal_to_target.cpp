b0bcb2c03945dce20b4be3a5a26b73e71eb837fe
Harshdeep Yadav <harshdeepy5708@gmail.com>
2022-07-27 23:30:16 +0530

    initial commit

--- Problems_on_subsequence_pattern.cpp/subsets_sum_equal_to_target.cpp
+++ Problems_on_subsequence_pattern.cpp/subsets_sum_equal_to_target.cpp
@@ -36,3 +36,51 @@ bool subsetSumToK(int n, int k, vector<int> &arr) {
     return f(n-1,k,arr,dp);
 }
 
+//`````````````````````tabulation solution``````````````````
+
+bool subsetSumToK(int n, int k, vector<int> &arr) {
+    vector<vector<bool>> dp(n,vector<bool>(k+1,0));
+    for(int i=0;i<n;i++) dp[i][0]=true; // base case 
+    dp[0][arr[0]]=true; // base case 
+    
+    for(int ind=1;ind<n;ind++){ //index
+        for(int target=1;target<k;target++) { // target
+
+            bool nottake=dp[ind-1][target];
+            bool take=false;
+            if(target>=arr[ind]) take=dp[ind-1][target-arr[ind]]; // recursion
+            
+            dp[ind][target]=take||nottake;
+        }
+    }
+    return dp[n-1][k];
+}
+
+
+// ```````````````space opt.```````````````````````````````````
+bool subsetSumToK(int n, int k, vector<int> &arr){
+    vector<bool> prev(k+1,false);
+    
+    prev[0] = true;
+    
+    if(arr[0]<=k)
+        prev[arr[0]] = true;
+    
+    for(int ind = 1; ind<n; ind++){
+        vector<bool> cur(k+1,false);
+        cur[0] = true;
+        for(int target= 1; target<=k; target++){
+            bool notTaken = prev[target];
+    
+            bool taken = false;
+                if(arr[ind]<=target)
+                    taken = prev[target-arr[ind]];
+        
+            cur[target]= notTaken||taken;
+        }
+        prev = cur;
+    }
+    
+    return prev[k];
+}
+
