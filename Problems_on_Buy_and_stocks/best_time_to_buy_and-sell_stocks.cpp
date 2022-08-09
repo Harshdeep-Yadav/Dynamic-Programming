7a5bf4925b13f526b165e5e7ea31f96b4b3ebd03
Harshdeep Yadav <harshdeepy5708@gmail.com>
2022-08-10 00:45:03 +0530

    initial commit

---
+++ Problems_on_Buy_and_stocks/best_time_to_buy_and-sell_stocks.cpp
@@ -0,0 +1,36 @@
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices){
+//         int n=prices.size();
+//         int profit=0;
+//         int min=prices[0];
+//         for(int i=1;i<n;i++)
+//         {
+//             if(prices[i]<min)
+//             {
+//                 min=prices[i];
+//             }
+//             else if(prices[i]-min>profit)
+//             {
+//                    profit=prices[i]-min ;
+//             }
+//         }
+//         return profit;
+//     }
+// };
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices){
+        int n=prices.size();
+        int profit=0;
+        int mini=prices[0];
+        for(int i=1;i<n;i++){
+           int cost=prices[i]-mini;
+           profit=max(profit,cost);
+           mini=min(prices(i),mini);
+        }
+        return profit;
+    }
+};
